080f36b8fef9eb4545f174f3b49324064645fcd9
mrashed21 <rashedjaman768@gmail.com>
2026-02-13 20:02:00 +0600

    add doctor schema

---
+++ prisma/migrations/20260213135840_doc_sp/migration.sql
@@ -0,0 +1,63 @@
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE');
+
+-- CreateTable
+CREATE TABLE "doctor" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "profilePhoto" TEXT,
+    "contactNumber" TEXT,
+    "address" TEXT,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "deletedAt" TIMESTAMP(3),
+    "registrationNumber" TEXT,
+    "experience" INTEGER NOT NULL DEFAULT 0,
+    "gender" "Gender" NOT NULL,
+    "appointmentFee" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "qualification" TEXT NOT NULL,
+    "currentWorkingPlace" TEXT NOT NULL,
+    "designation" TEXT NOT NULL,
+    "avarageRating" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "totalRating" INTEGER NOT NULL DEFAULT 0,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "doctor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "doctor_specialty" (
+    "id" TEXT NOT NULL,
+    "doctorId" TEXT NOT NULL,
+    "specialtyId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "doctor_specialty_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "doctor_email_key" ON "doctor"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "doctor_userId_key" ON "doctor"("userId");
+
+-- CreateIndex
+CREATE INDEX "doctor_email_idx" ON "doctor"("email");
+
+-- CreateIndex
+CREATE INDEX "doctor_isDeleted_idx" ON "doctor"("isDeleted");
+
+-- CreateIndex
+CREATE INDEX "doctor_specialty_idx" ON "doctor_specialty"("doctorId", "specialtyId");
+
+-- AddForeignKey
+ALTER TABLE "doctor" ADD CONSTRAINT "doctor_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "doctor_specialty" ADD CONSTRAINT "doctor_specialty_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "doctor"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "doctor_specialty" ADD CONSTRAINT "doctor_specialty_specialtyId_fkey" FOREIGN KEY ("specialtyId") REFERENCES "specialties"("id") ON DELETE CASCADE ON UPDATE CASCADE;
