89fe06b891d2e0078dab698a075792391894d7b4
mrashed21 <rashedjaman768@gmail.com>
2026-02-24 00:49:24 +0600

    add admin seed

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -25,6 +25,8 @@ interface IConfig {
   CLOUDINARY_API_SECRET: string;
   STRIPE_SECRET_KEY: string;
   STRIPE_WEBHOOK_SECRET: string;
+  SUPER_ADMIN_EMAIL: string;
+  SUPER_ADMIN_PASSWORD: string;
 }
 
 const requiredEnv = [
@@ -49,6 +51,8 @@ const requiredEnv = [
   "CLOUDINARY_API_SECRET",
   "STRIPE_SECRET_KEY",
   "STRIPE_WEBHOOK_SECRET",
+  "SUPER_ADMIN_EMAIL",
+  "SUPER_ADMIN_PASSWORD",
 ];
 
 requiredEnv.forEach((variable) => {
@@ -82,4 +86,6 @@ export const config: IConfig = {
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET!,
   STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY!,
   STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET!,
+  SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL!,
+  SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD!,
 };
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import app from "./app";
 import { config } from "./app/config/config";
+import { seedSuperAdmin } from "./app/utils/seed";
 
-const server = () => {
+const server = async () => {
   try {
+    await seedSuperAdmin();
     app.listen(config.PORT, () => {
       console.log(`Server is running on http://localhost:${config.PORT}`);
     });
